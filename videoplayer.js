064c0472edd062a162fd226f33add2dc1c21015c
JairPPereira <jairppereira21@gmail.com>
2021-09-09 17:48:55 -0300

    mtv

---
+++ videoplayer.js
@@ -0,0 +1,9 @@
+var player= videojs('video', {autoplay:'muted',
+constrols: true,  width:"1282",
+height:"720",
+
+poster:'https://1.bp.blogspot.com/-cAIfauhtlrQ/YS5nAH3D0ZI/AAAAAAAAA-0/3AIe5V9APag9U4auOEJK2VUfPmJBVXm7QCLcBGAsYHQ/s0/bandnews200.jpg',
+Plugins: {hotkeys: {
+    enableModifiersForNumbers: false, seekStep: 30
+},}
+});
