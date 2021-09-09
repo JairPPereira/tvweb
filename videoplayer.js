3a850fe793a5f2873ae9bd661351ed9c7b783b2d
JairPPereira <jairppereira21@gmail.com>
2021-09-09 17:57:26 -0300

    mtv

--- videoplayer.js
+++ videoplayer.js
@@ -2,7 +2,7 @@ var player= videojs('video', {autoplay:'muted',
 constrols: true,  width:"1282",
 height:"720",
 
-poster:'https://1.bp.blogspot.com/-cAIfauhtlrQ/YS5nAH3D0ZI/AAAAAAAAA-0/3AIe5V9APag9U4auOEJK2VUfPmJBVXm7QCLcBGAsYHQ/s0/bandnews200.jpg',
+poster:'https://1.bp.blogspot.com/-dM7_KJvuS-4/YTpz8gKF_7I/AAAAAAAABDQ/Q6aIFxbfTbEVYIx1uxnG31tZYotATgCmwCLcBGAsYHQ/s0/mtv200.png',
 Plugins: {hotkeys: {
     enableModifiersForNumbers: false, seekStep: 30
 },}
