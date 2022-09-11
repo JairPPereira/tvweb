b32e7571fd99ed030e9d615008e9b498b1a86b12
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-11 17:31:10 -0300

    l

---
+++ Liberalfm/pt.js
@@ -0,0 +1,92 @@
+'use strict';/*!
+ * This is a `i18n` language object.
+ *
+ * Portuguese
+ *
+ * @author
+ *   Jalios (Twitter: @Jalios)
+ *   Sascha Greuel (Twitter: @SoftCreatR)
+ *
+ * @see core/i18n.js
+ */
+(function (exports) {
+	if (exports.pt === undefined) {
+		exports.pt = {
+			'mejs.plural-form': 1,			
+			'mejs.download-file': 'Descarregar o ficheiro',			
+			'mejs.install-flash': 'VocÃª estÃ¡ usando um navegador que nÃ£o possui o player Flash ativado ou instalado. Por favor, ligue o plugin do Flash Player ou baixe a versÃ£o mais recente de https://get.adobe.com/flashplayer/',			
+			'mejs.fullscreen': 'EcrÃ£ completo',			
+			'mejs.play': 'ReproduÃ§Ã£o',
+			'mejs.pause': 'Pausa',			
+			'mejs.time-slider': 'Deslizador do tempo',
+			'mejs.time-help-text': 'Use as teclas das setas para a esquerda/direita para avanÃ§ar um segundo, e as setas para cima/baixo para avanÃ§ar dez segundos.',
+			'mejs.live-broadcast' : 'TransmissÃ£o ao vivo',
+			
+			
+			'mejs.volume-help-text': 'Use as teclas das setas para cima/baixo para aumentar ou diminuir o volume.',
+			'mejs.unmute': 'Voltar ao som',
+			'mejs.mute': 'SilÃªncio',
+			'mejs.volume-slider': 'Deslizador do volume',			
+			'mejs.video-player': 'Leitor de vÃ­deo',
+			'mejs.audio-player': 'Leitor de Ã¡udio',			
+			'mejs.captions-subtitles': 'Legendas',
+			'mejs.captions-chapters': 'CapÃ­tulos',
+			'mejs.none': 'Nenhum',
+			'mejs.afrikaans': 'Afrikaans',
+			'mejs.albanian': 'AlbanÃªs',
+			'mejs.arabic': 'Ãrabe',
+			'mejs.belarusian': 'Bielorrusso',
+			'mejs.bulgarian': 'BÃºlgaro',
+			'mejs.catalan': 'CatalÃ£o',
+			'mejs.chinese': 'ChinÃªs',
+			'mejs.chinese-simplified': 'Chinese (Simplified)',
+			'mejs.chinese-traditional': 'Chinese (Traditional)',
+			'mejs.croatian': 'Croata',
+			'mejs.czech': 'Checo',
+			'mejs.danish': 'Danish',
+			'mejs.dutch': 'Dutch',
+			'mejs.english': 'InglÃªs',
+			'mejs.estonian': 'Estoniano',
+			'mejs.filipino': 'Filipino',
+			'mejs.finnish': 'FinlandÃªs',
+			'mejs.french': 'French',
+			'mejs.galician': 'Galego',
+			'mejs.german': 'AlemÃ£o',
+			'mejs.greek': 'Grego',
+			'mejs.haitian-creole': 'Crioulo Haitiano',
+			'mejs.hebrew': 'Hebraico',
+			'mejs.hindi': 'Hindi',
+			'mejs.hungarian': 'HÃºngaro',
+			'mejs.icelandic': 'IslandÃªs',
+			'mejs.indonesian': 'IndonÃ©sio',
+			'mejs.irish': 'Irish',
+			'mejs.italian': 'Italiano',
+			'mejs.japanese': 'JaponÃªs',
+			'mejs.korean': 'Coreano',
+			'mejs.latvian': 'LetÃ£o',
+			'mejs.lithuanian': 'Lithuanian',
+			'mejs.macedonian': 'MacedÃ´nio',
+			'mejs.malay': 'Malaio',
+			'mejs.maltese': 'MaltÃªs',
+			'mejs.norwegian': 'Norwegian',
+			'mejs.persian': 'Persa',
+			'mejs.polish': 'Polish',
+			'mejs.portuguese': 'PortuguÃªs',
+			'mejs.romanian': 'Romanian',
+			'mejs.russian': 'Russian',
+			'mejs.serbian': 'SÃ©rvio',
+			'mejs.slovak': 'Slovak',
+			'mejs.slovenian': 'Slovenian',
+			'mejs.spanish': 'Espanhol',
+			'mejs.swahili': 'Swahili',
+			'mejs.swedish': 'sueco',
+			'mejs.tagalog': 'Tagalog',
+			'mejs.thai': 'Thai',
+			'mejs.turkish': 'Turco',
+			'mejs.ukrainian': 'Ucraniano',
+			'mejs.vietnamese': 'Vietnamita',
+			'mejs.welsh': 'Welsh',
+			'mejs.yiddish': 'IÃ­diche'
+		};
+	}
+})(mejs.i18n);
