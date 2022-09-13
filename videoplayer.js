fb85b711c5a247e435401332e7f679fac11b96d4
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-09-12 21:10:03 -0300

    bbb

--- videoplayer.js
+++
@@ -1,9 +0,0 @@
-var player= videojs('video', {autoplay:'true',
-constrols: true,  width:"1282",
-height:"720",
-
-poster:'https://1.bp.blogspot.com/-dM7_KJvuS-4/YTpz8gKF_7I/AAAAAAAABDQ/Q6aIFxbfTbEVYIx1uxnG31tZYotATgCmwCLcBGAsYHQ/s0/mtv200.png',
-Plugins: {hotkeys: {
-    enableModifiersForNumbers: false, seekStep: 30
-},}
-});
