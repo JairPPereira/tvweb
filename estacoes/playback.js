d319a5ec46a98afec7b0f38a2e5c906587c09c67
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-10-23 13:48:09 -0300

    hh

--- estacoes/playback.js
+++ estacoes/playback.js
@@ -1,15 +1,17 @@
 const sources = [
   "https://stream-41.zeno.fm/2cgy8mzvsy8uv?zs=yo4PyEhfTWK29YRfVvNeWA", // passado
   "https://stream-59.zeno.fm/p3yys99taphvv?zs=YmiO2TgATMaHAKx7t057pw", // top hits
-  "http://ice2.somafm.com/folkfwd-128-mp3", // folk forward
-  "http://94.23.26.22:8090/live.mp3" // punk fm
+  "https://stream-44.zeno.fm/9t6tfnsn3ehvv?zs=59Ws6zggTjSMI7cMkwtnFg", // Atualidade
+  "https://stream-57.zeno.fm/9ts1tgxryhhvv?zs=cwpQYLFdTxmVOaOVAJ_rYQ&aw_0_req_lsid=1663871100546_0.44668418486659633&an-uid=8773777118362868920&triton-uid=cookie%3Ab9e998c5-ed73-45ba-983a-4fef71654a75", // Pop Rock
+  "https://stream-41.zeno.fm/yyv0vgxz4rhvv?zs=xP4k0slFQTmUc4oKWb86hg&aw_0_req_lsid=1665535484228_0.3879091453241035&an-uid=7245416173759505668&triton-uid=cookie%3A6e29eb36-65fb-44bd-a64e-9f6c50e9599c", // Harpa cristã
 ];
 
 const labels = [
   [ "Radio Passado", "https://stream-41.zeno.fm/2cgy8mzvsy8uv?zs=yo4PyEhfTWK29YRfVvNeWA" ],
   [ "Top hits", "https://stream-59.zeno.fm/p3yys99taphvv?zs=YmiO2TgATMaHAKx7t057pw"],
-  [ "SomaFM Folk Forward", "http://somafm.com/folkfwd/"],
-  [ "Punk FM", " http://www.punkfm.co.uk/"]
+  [ "Atualidade", "https://stream-44.zeno.fm/9t6tfnsn3ehvv?zs=59Ws6zggTjSMI7cMkwtnFg"],
+  [ "Pop Rock", "https://stream-57.zeno.fm/9ts1tgxryhhvv?zs=cwpQYLFdTxmVOaOVAJ_rYQ&aw_0_req_lsid=1663871100546_0.44668418486659633&an-uid=8773777118362868920&triton-uid=cookie%3Ab9e998c5-ed73-45ba-983a-4fef71654a75"],
+  [ "Harpa Cristã", "https://stream-41.zeno.fm/yyv0vgxz4rhvv?zs=xP4k0slFQTmUc4oKWb86hg&aw_0_req_lsid=1665535484228_0.3879091453241035&an-uid=7245416173759505668&triton-uid=cookie%3A6e29eb36-65fb-44bd-a64e-9f6c50e9599c"]
 ];
 
 let playingIndex = 0; // current radio
