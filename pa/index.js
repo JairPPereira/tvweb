bab1cc0f4fdabbaca5a868d1e9cd52221183652c
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-11-03 13:21:19 -0300

    p

---
+++ pa/index.js
@@ -0,0 +1,14 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display(displayValorAnterior, displayValorActual);
+
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
+
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
