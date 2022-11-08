3d7e93bafaa6de4c320aaf2308a36a50aaad1c51
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-11-08 20:51:57 -0300

    ss

---
+++ calculadoras/simples/Display.js
@@ -0,0 +1,55 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            dividir: '%',
+            multiplicar: 'x',
+            restar: '-', 
+        }
+    }
+
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrarTodo() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+
+    agregarNumero(numero) {
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+    }
+
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual)  || isNaN(valorAnterior) ) return
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+    }
+}
