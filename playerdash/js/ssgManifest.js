bb7d12d9032212509d9ca28d7df19a3af50ee768
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-11-14 07:10:42 -0300

    dd

---
+++ playerdash/js/ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fversion","\u002Ffaq","\u002F","\u002Fprivacy-policy","\u002Fcatalog\u002F[id]\u002F[slug]","\u002Fchannel\u002F[id]\u002F[slug]","\u002Fmedia\u002F[mediaType]\u002F[id]\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
