4e2f0f77027b59fa99d259a30ca87f0eb2a8144f
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-05-04 22:44:01 -0300

    Create index.php

---
+++ discoverys/index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>	<script>
+if (window.top == window.self) {
+	window.location.href = "https://jptv.netlify.app/discoverys/";
+}
+</script><meta name="robots"content="noindex,nofollow">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="cache-control"content="no-cache"/><meta http-equiv="pragma"content="no-cache"/>	
+<title>Player</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+</head>
+<style type="text/css">	
+#iframeVideo{
+    position:absolute;
+    left:0;
+    z-index: 9999;
+    aspect-ratio: 16/9;
+    width:100%;
+    margin:0;
+    border:none;
+    padding:none;
+    background: #000 url(https://onlinetvhd.net/imagens/loading.gif) no-repeat center center fixed;
+}	
+	</style>
+	<style>.btn-atualizar { background-color: #063252; border-top-left-radius: 0px !important;border-top-right-radius: 0px !important; color: white;padding: 10px 12px;margin: 0px;margin-left:10px;border: none;border-radius: 5px;float:left;cursor: pointer;font-family: "Verdana", Arial;font-size:12px;}.btn-lista {background-color: #4286f4 !important;}.btn-lista:hover {background-color: #3671d1 !important;}.btn-atualizar:hover {background-color: #0000ff;}.div-btns {margin: 0px;padding:0;position: absolute;left: 0px;top: 0px;z-index:99999;}</style>	
+<body><div class="div-btns"><div class="btn-atualizar" onclick="location.reload(true)"><i class="fas fa-sync-alt"></i><span style="margin-left:5px;">Travou? Clique Aqui!</span></div></div>	
+	<iframe src="https://player.aovivotv.xyz/channels/cnnbrasil" id="iframeVideo" name="iframeVideo" scrolling="no" sandbox="allow-same-origin allow-scripts" allowfullscreen autoplay></iframe>
+</div>
+</body>
+</html>
