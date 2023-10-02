e10c43884a70716b0d569d1628b0a1fd56f98c73
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-10-02 19:25:46 -0300

    hjj

--- jogosdj/jogos.py
+++ jogosdj/jogos.py
@@ -1,6 +1,5 @@
-
-
-
+# Sua lista de texto
+lista_texto = """
 07:00	Internazionale U19 x Benfica U19 | https://sportsonline.so/channels/pt/eleven1.php
 09:00	Napoli U19 x Real Madrid U19 | https://sportsonline.so/channels/pt/eleven1.php
 11:00	PSV U19 x Sevilla U19 | https://sportsonline.so/channels/pt/eleven1.php
@@ -13,4 +12,16 @@
 19:30	Mirassol x Londrina | https://sportsonline.so/channels/bra/br2.php
 21:30	Fortaleza x Corinthians | https://sportsonline.so/channels/bra/br1.php
 21:30	Fortaleza x Corinthians | https://sportsonline.so/channels/bra/br3.php
+"""
 
+# Divide as linhas em uma lista
+linhas = lista_texto.strip().split('\n')
+
+# Loop através das linhas e gere o formato desejado
+for linha in linhas:
+    partes = linha.split('|')
+    horario_equipe = partes[0].strip()
+    link = partes[1].strip()
+
+    # Imprima o formato desejado
+    print(f'<button style="width: 95%; margin: 10px; font-size: 20px; font-weight: bold;" class="waves-effect waves-light btn-large" onclick="openMovie(\'{horario_equipe}\', \'{link}\')">{horario_equipe}</button>')
