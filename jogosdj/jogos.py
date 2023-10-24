b83d4284d2a3ecd3ed69293d4486284ad3345de9
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2023-10-24 13:54:16 -0300

    jj

--- jogosdj/jogos.py
+++ jogosdj/jogos.py
@@ -1,40 +1,30 @@
 # Sua lista de texto
 lista_texto = """
-04:30   ATP World Tour 500: Final | https://v2.sportsonline.so/channels/pt/sporttv2.php
-04:40   MLMC: Portugal | https://v2.sportsonline.so/channels/pt/sporttv4.php
-07:15   Utrecht x Ajax | https://v2.sportsonline.so/channels/pt/sporttv1.php
-07:30   Roma x Monza | https://v2.sportsonline.so/channels/bra/br4.php
-08:00   Lorient x Rennes | https://v2.sportsonline.so/channels/pt/eleven1.php
-08:00   DP World Tour: Andalucia Masters - D4 | https://v2.sportsonline.so/channels/pt/sporttv3.php
-08:30   Nürnberg x Hertha BSC | https://v2.sportsonline.so/channels/pt/eleven2.php
-09:00   Las Palmas x Rayo Vallecano | https://v2.sportsonline.so/channels/pt/eleven3.php
-10:00   Salernitana x Cagliari | https://v2.sportsonline.so/channels/pt/sporttv2.php
-10:00   Lille x Brest | https://v2.sportsonline.so/channels/pt/eleven1.php
-10:15   Hearts x Celtic | https://v2.sportsonline.so/channels/pt/sporttv1.php
-10:30   Köln x Borussia M'gladbach | https://v2.sportsonline.so/channels/pt/eleven2.php
-11:15   Girona x Almería | https://v2.sportsonline.so/channels/pt/eleven3.php
-12:05   Monaco x Metz | https://v2.sportsonline.so/channels/bra/br3.php
-12:30   Aston Villa x West Ham United | https://v2.sportsonline.so/channels/bra/br4.php
-12:30   Heidenheim x Augsburg | https://v2.sportsonline.so/channels/pt/eleven2.php
-13:00   Atalanta x Genoa | https://v2.sportsonline.so/channels/bra/br5.php
-13:00   Fenerbahçe x Hatayspor | https://v2.sportsonline.so/channels/pt/sporttv2.php
-13:30   Villarreal x Deportivo Alavés | https://v2.sportsonline.so/channels/pt/eleven3.php
-15:30   NASCAR Cup Series: Homestead - Miami Speedway | https://v2.sportsonline.so/channels/pt/sporttv5.php
-15:45   Olympique Lyonnais x Clermont | https://v2.sportsonline.so/channels/pt/eleven2.php
-15:45   Milan x Juventus | https://v2.sportsonline.so/channels/bra/br5.php
-16:00   Barcelona x Athletic Club | https://v2.sportsonline.so/channels/bra/br4.php
-16:00   UD Oliveirense x Boavista | https://v2.sportsonline.so/channels/pt/sporttv1.php
-16:00   Flamengo x Vasco da Gama | https://v2.sportsonline.so/channels/bra/br1.php
-16:00   Atlético Mineiro x Cruzeiro | https://v2.sportsonline.so/channels/bra/br2.php
-16:00   Internacional x Santos | https://v2.sportsonline.so/channels/bra/br3.php
-16:00   F1: USA Grand Prix | https://v2.sportsonline.so/channels/pt/sporttv4.php
-17:25   NFL: Los Angeles Chargers @ Kansas City Chiefs | https://v2.sportsonline.so/channels/pt/eleven3.php
-18:00   Avaí x Ceará | https://v2.sportsonline.so/channels/bra/br4.php
-18:00   Sport x Chapecoense | https://v2.sportsonline.so/channels/bra/br5.php
-18:30   Corinthians x América Mineiro | https://v2.sportsonline.so/channels/bra/br1.php
-18:30   Coritiba x Palmeiras | https://v2.sportsonline.so/channels/bra/br2.php
-18:30   RB Bragantino x Fluminense | https://v2.sportsonline.so/channels/bra/br3.php
-21:20   NFL: Miami Dolphins @ Philadelphia Eagles | https://v2.sportsonline.so/channels/bra/br5.php
+05:00   DP World Tour: Qatar Masters - D1 | https://v2.sportsonline.so/channels/pt/sporttv3.php
+09:00   ATP World Tour 500: Basel | https://v2.sportsonline.so/channels/pt/sporttv2.php
+09:00   WRC: Central European Rally - Super Special 1 | https://v2.sportsonline.so/channels/pt/sporttv4.php
+10:00   Judo: Grand Slam of Abu Dhabi - D3 | https://v2.sportsonline.so/channels/pt/sporttv5.php
+13:00   WRC: Central European Rally - Super Special 2 | https://v2.sportsonline.so/channels/pt/sporttv4.php
+13:45   Olympiakos Piraeus x West Ham United | https://v2.sportsonline.so/channels/bra/br5.php
+13:45   AZ x Aston Villa | https://v2.sportsonline.so/channels/bra/br3.php
+13:45   Lille x Slovan Bratislava | https://v2.sportsonline.so/channels/pt/eleven3.php
+13:45   Sturm Graz x Atalanta | https://v2.sportsonline.so/channels/pt/sporttv2.php
+13:45   Olympique Marseille x AEK Athens | https://v2.sportsonline.so/channels/pt/sporttv3.php 
+13:45   Raków Częstochowa x Sporting CP | https://v2.sportsonline.so/channels/bra/br2.php
+15:00   Al Ittihad x Al Hazm | https://v2.sportsonline.so/channels/pt/sporttv4.php
+16:00   Liverpool x Toulouse | https://v2.sportsonline.so/channels/bra/br1.php
+16:00   Brighton & Hove Albion x Ajax | https://v2.sportsonline.so/channels/bra/br4.php
+16:00   Bayer Leverkusen x Qarabağ | https://v2.sportsonline.so/channels/bra/br3.php
+16:00   Aberdeen x PAOK | https://v2.sportsonline.so/channels/pt/eleven3.php
+16:00   Roma x Slavia Praha | https://v2.sportsonline.so/channels/bra/br5.php
+19:00   Vasco da Gama x Internacional | https://v2.sportsonline.so/channels/bra/br1.php
+19:00   Vasco da Gama x Internacional | https://v2.sportsonline.so/channels/bra/br2.php
+19:00   ABC x Avaí | https://v2.sportsonline.so/channels/bra/br3.php
+20:30   NBA: Philadelphia 76ers @ Milwaukee Bucks | https://v2.sportsonline.so/channels/pt/sporttv2.php
+21:20   NFL: Tampa Bay Buccaneers @ Buffalo Bills | https://v2.sportsonline.so/channels/pt/eleven1.php
+21:30   Santos x Coritiba | https://v2.sportsonline.so/channels/bra/br1.php
+21:30   Santos x Coritiba | https://v2.sportsonline.so/channels/bra/br2.php
+23:00   MotoGP: GP of Thailand FP1 | https://v2.sportsonline.so/channels/bra/br5.php
 """
 
 # Divide as linhas em uma lista
