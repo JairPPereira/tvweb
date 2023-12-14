ed4780f78e72ac854f64e4bb12f48edc518630a1
JPP <86321461+JairPPereira@users.noreply.github.com>
2023-12-14 09:25:46 -0300

    hhh

--- jpiptv/pornolandia.py
+++ jpiptv/pornolandia.py
@@ -21,6 +21,6 @@ def processar_url(url):
         return 'URL não corresponde ao padrão esperado.'
 
 # Exemplo de uso
-url_input = "https://www.pornolandia.xxx/video/53663/brasileirinha-dando-o-cu-de-4"
+url_input = "https://www.pornolandia.xxx/video/53620/novinha-branquinha-nua-dando-de-pernas-abertas"
 saida_formatada = processar_url(url_input)
 print(saida_formatada)
